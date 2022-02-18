221710db747bf4134052d85a4c413d19b053efe4
AmmarCode <ammar.code01@gmail.com>
2022-02-18 12:18:08 -0500

    Add images controls on sm screen

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let expandImg = document.getElementById("expandedImg");
-    if (! expandImg.src) {
-        expandImg.src = "./images/image-product-1.jpg"
-    }
+  let expandImg = document.getElementById("expandedImg");
+  const next = document.getElementById("next");
+  const previous = document.getElementById("previous");
+  const images = document.querySelectorAll(".thumbnail-img");
 
-    const next = document.getElementsByClassName('next');
-    const previous = document.getElementsByClassName('previous');
-
-    next.addEventListener('click', (e) => {
-        expandImg.src = `./images/`
-    })
-
-
-})
-
-function expandImg(imgs) {
-    let expandImg = document.getElementById("expandedImg");
-    expandImg.src = imgs.src;
-    expandImg.parentElement.style.display = "block";
+  if (!expandImg.src) {
+    expandImg.src = "./images/image-product-1.jpg";
   }
 
+  images.forEach((img) => {
+    img.addEventListener("click", () => {
+      expandImg.src = img.src;
+      expandImg.parentElement.style.display = "block";
+    });
+  });
+
+  next.addEventListener("click", () => {
+      currentImg = expandImg.src;
+      let i = 0
+    while (i < images.length - 1) {
+      if (currentImg === images[i].src) {
+        expandImg.src = images[i+1].src
+      }
+      i++
+    }
+  });
+
+  previous.addEventListener("click", () => {
+    currentImg = expandImg.src;
+    let i = images.length - 1;
+    while (i > 0) {
+        if (currentImg === images[i].src) {
+            expandImg.src = images[i-1].src
+          }
+          i--
+    }
+    // for (let i = images.length - 1; i > -1; i--) {
+    //     console.log(i)
+    //     if (currentImg === images[i].src) {
+    //       console.log(images[i].src)
+    //       expandImg.src = images[i-1].src
+    //       console.log(expandImg.src)
+    //     }
+    //   }
+  });
+});
